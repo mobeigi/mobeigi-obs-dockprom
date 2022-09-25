23219c19f4ed60e29d185ef82d38b8e98413f588
Mo Beigi <me@mobeigi.com>
2022-09-25 20:59:23 +1000

    Remove broken pushbullet integration for now

--- compose.sh
+++ compose.sh
@@ -3,7 +3,6 @@
 # Check env vars are set
 if [[ -z "${ADMIN_USER}" ]]; then echo "ADMIN_USER is not set" && exit 1; fi;
 if [[ -z "${ADMIN_PASSWORD}" ]]; then echo "ADMIN_PASSWORD is not set" && exit 1; fi;
-if [[ -z "${PUSHBULLET_API_KEY}" ]]; then echo "PUSHBULLET_API_KEY is not set" && exit 1; fi;
 if [[ -z "${ALERTMANAGER_SMTP_HOST}" ]]; then echo "ALERTMANAGER_SMTP_HOST is not set" && exit 1; fi;
 if [[ -z "${ALERTMANAGER_SMTP_USERNAME}" ]]; then echo "ALERTMANAGER_SMTP_USERNAME is not set" && exit 1; fi;
 if [[ -z "${ALERTMANAGER_SMTP_PASSWORD}" ]]; then echo "ALERTMANAGER_SMTP_PASSWORD is not set" && exit 1; fi;
